4bb63d6723b0b11e69ae4ef26a94f4be11b74eb2
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-11-21 16:07:55 -0700

    Add project files.

---
+++ ArrayListExerciseOne/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrayListExerciseOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
